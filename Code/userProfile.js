53de6e1743067d02fb67d934be9ff2e67204b4a9
lakshyajai <laja3167@colorado.edu>
2020-10-07 19:32:10 -0600

    current profile page

--- Code/userProfile.js
+++ Code/userProfile.js
@@ -1,6 +1,23 @@
-$("#profileImage").click(function(e) {
-    $("#imageUpload").click();
-  });
+// $("#profileImage").click(function(e) {
+//     $("#imageUpload").click();
+//   });
 
-  document.querySelector('input[type=file]').addEventListener('change', function(){
-    this.form.submit()});
+//   document.querySelector('input[type=file]').addEventListener('change', function(){
+//     this.form.submit()});
+
+var user = "./assets/profile_pic_placeholder.gif";
+function uploadImage(){
+    var imageString = '<image\
+    id="profileImage"\
+    src="' + user + '/>\
+    style="margin-left: 0px"\>';
+
+    var container = document.createElement("div");
+    container.classList.add("container");
+    container.innerHTML = imageString;
+
+    var here = document.getElementById("profile_picture");
+    here.appendChild(container);
+}
+
+
