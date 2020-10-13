bc54487ee4f59e43907ebdaad2e8e4926afa06dd
sama9843 <sama9843@colorado.edu>
2020-10-13 17:29:34 -0600

    Updated database

--- envi-api/database/database2.sql
+++ envi-api/database/database2.sql
@@ -11,31 +11,31 @@ CREATE TABLE users (
 );
 
 -- -- creates linking table to array of friends
--- CREATE TABLE friend_link (
---     user_id INT NOT NULL, 
---     friends_id_array int[], 
---     FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
--- );
+CREATE TABLE friend_link (
+    user_id INT NOT NULL, 
+    friends_id_array int[], 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
--- CREATE TABLE actions_link (
---     user_id INT NOT NULL, 
---     actions_id_array int[], 
---     FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
--- );
+CREATE TABLE actions_link (
+    user_id INT NOT NULL, 
+    actions_id_array int[], 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
--- CREATE TABLE achievments (
---     user_id INT NOT NULL, 
---     achievments_id_array int[], 
---     FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
--- );
+CREATE TABLE achievments (
+    user_id INT NOT NULL, 
+    achievments_id_array int[], 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
--- CREATE TABLE  achievments_list (
---     achievments_id INT PRIMARY KEY NOT NULL, 
---     achievment_name VARCHAR NOT NULL
--- );
+CREATE TABLE  achievments_list (
+    achievments_id INT PRIMARY KEY NOT NULL, 
+    achievment_name VARCHAR NOT NULL
+);
 
--- CREATE TABLE actions_list (
---     actions_id INT NOT NULL, 
---     action_name VARCHAR NOT NULL
--- );
+CREATE TABLE actions_list (
+    actions_id INT NOT NULL, 
+    action_name VARCHAR NOT NULL
+);
 
