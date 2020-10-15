480beaaf7ebeca7aefb0010154c7fb89558b7bca
Matthew Teta <matthewtetadev@gmail.com>
2020-10-14 21:21:15 -0600

    Removed some bugs from signup and login, added completeTast route, changed schema

---
+++ envi-api/src/config/passport.ts
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt')
+const LocalStrategy = require('passport-local').Strategy
+
+module.exports = (passport, query) => {
+    // Begin by setting up local login with postgres db
+    passport.use(new LocalStrategy(
+        async (username, password, done) => {
+            const results = await query('SELECT * FROM users WHERE username=$1', [username])
+            if (!results || results.rows.length == 0) {
+                let err = "User not found"
+                console.error(err)
+                return done(err)
+            }
+            const user = results.rows[0];
+            bcrypt.compare(password, user.password, function(err, res) {
+                
+                if(err) {
+                    console.error(err)
+                    done(err)
+                } else if (res) {
+                    done(null, { id: user.id, username: user.username })
+                }
+            })
+        }
+        ));
+        
+        passport.serializeUser((user, done) => {
+            done(null, user.id)
+        })
+        
+        passport.deserializeUser((id, done) => {
+            query('SELECT id, username FROM users WHERE id = $1', [parseInt(id, 10)], (err, results) => {
+                if(err) {
+                    console.error('Error when selecting user on session deserialize', err)
+                    return done(err)
+                }
+                
+                done(null, results.rows[0])
+            })
+        })
+    }
+    
+    
