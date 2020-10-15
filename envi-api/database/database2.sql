e520c72f38f7e7d48b6813ac1530424b7e482c94
Matthew Teta <matthewtetadev@gmail.com>
2020-10-14 18:34:45 -0600

    Added image_id to user table

--- envi-api/database/database2.sql
+++ envi-api/database/database2.sql
@@ -6,36 +6,37 @@ CREATE TABLE users (
     firstname VARCHAR NOT NULL, 
     lastname VARCHAR NOT NULL, 
     email VARCHAR NOT NULL, 
-    birthday VARCHAR, 
+    image_id INT,
+    birthday DATE, 
     signupdate timestamp default current_timestamp
 );
 
--- -- creates linking table to array of friends
--- CREATE TABLE friend_link (
---     user_id INT NOT NULL, 
---     friends_id_array int[], 
---     FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
--- );
+-- creates linking table to array of friends
+CREATE TABLE friend_link (
+    user_id INT NOT NULL, 
+    friends_id_array int[], 
+    FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
+);
 
--- CREATE TABLE actions_link (
---     user_id INT NOT NULL, 
---     actions_id_array int[], 
---     FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
--- );
+CREATE TABLE actions_link (
+    user_id INT NOT NULL, 
+    actions_id_array int[], 
+    FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
+);
 
--- CREATE TABLE achievments (
---     user_id INT NOT NULL, 
---     achievments_id_array int[], 
---     FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
--- );
+CREATE TABLE achievments (
+    user_id INT NOT NULL, 
+    achievments_id_array int[], 
+    FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
+);
 
--- CREATE TABLE  achievments_list (
---     achievments_id INT PRIMARY KEY NOT NULL, 
---     achievment_name VARCHAR NOT NULL
--- );
+CREATE TABLE  achievments_list (
+    achievments_id INT PRIMARY KEY NOT NULL, 
+    achievment_name VARCHAR NOT NULL
+);
 
--- CREATE TABLE actions_list (
---     actions_id INT NOT NULL, 
---     action_name VARCHAR NOT NULL
--- );
+CREATE TABLE actions_list (
+    actions_id INT NOT NULL, 
+    action_name VARCHAR NOT NULL
+);
 
