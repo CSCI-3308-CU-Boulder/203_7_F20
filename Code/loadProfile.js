22249edb8ec1aa0cbc7cc586aee8d93c4a614624
Sarah Zendle <sarahzendle@Sarahs-MacBook-Pro-3.local>
2020-10-27 23:45:32 -0600

    adding potential stylesheet (only applied to profile page)

--- Code/loadProfile.js
+++ Code/loadProfile.js
@@ -3,17 +3,17 @@ var exAch = [
   {
     name: "Demo Achievement 1",
     description: "demo description text 1",
-    image: "./assets/envi.png",
+    image: "./assets/flatirons.png",
   },
   {
     name: "Demo Achievement 2",
     description: "demo description text 2",
-    image: "./assets/envi.png",
+    image: "./assets/mountains.png",
   },
   {
     name: "Demo Achievement 3",
     description: "demo description text 3",
-    image: "./assets/envi.png",
+    image: "./assets/ice.jpg",
   },
 ];
 
@@ -22,12 +22,12 @@ var exUser = {
   name: "Example User",
   num_bottles: 1,
   achievements: exAch,
-  image_id: 3,
+  image_id: 2,
 };
 
 var images = [
   "./assets/colorEarth.jpg",
-  "./assets/envi.png",
+  "./assets/recycling.jpg",
   "./assets/environmentalist.jpg",
   "./assets/flatirons.png",
   "./assets/flowers.jpg",
@@ -35,23 +35,25 @@ var images = [
   "./assets/ice.jpg",
   "./assets/mountains.png",
   "./assets/plant.jpg",
-  "./assets/recycling.jpeg",
+  "./assets/flatirons.png",
 ];
 
 function createAchievement(ach) {
   //builds string to insert card into html
   var cardStr =
-    '<div class = "card flex-row flex-wrap">\
-    <div class="card-header border-0">\
-      <img src="' +
+    '<div class = "card flex-row flex-wrap bg-lightblue" style="border: hidden; margin-bottom: 10px; border-radius: calc(0.75rem - 1px)">\
+    <div class="card-header" style="background-color: transparent; padding-top: 0.5rem;\
+    padding-right: 0.25rem;\
+    padding-bottom: 0rem;\
+    padding-left: 0.75rem;">\
+      <img style="width: 75px; border-radius:50%" src="' +
     ach.image +
-    '" width="50" alt="" />\
+    '" alt="" />\
     </div>\
-    <div class="card-block px-2">\
-      <h5 class="card-title">' +
+    <div class="card-block px-2 bac">\
+      <p class="card-title" style="padding-top: 12px"> <b>' +
     ach.name +
-    '</h5>\
-      <p class="card-text">' +
+    "</b> </br>" +
     ach.description +
     '</p>\
     </div>\
@@ -62,10 +64,9 @@ function createAchievement(ach) {
 
 function displayAchievements(user) {
   //builds container with dynamic amount of cards depending on user data
-  var achContent = "</br> <h3> My Achievements </h3> ";
+  var achContent = '</br> <div class="custom-header"> My Achievements </div> ';
   for (i = 0; i < user.achievements.length; i++) {
     var cardContent = createAchievement(user.achievements[i]);
-    achContent += "</br>";
     achContent += cardContent;
   }
 
@@ -77,6 +78,7 @@ function displayPicture(user) {
     id="profile-pic"
     width="500"
     src="${images[user.image_id]}"
+    class="userimg"
   />`;
   console.log(pic_string);
 
@@ -85,23 +87,24 @@ function displayPicture(user) {
 
 function displayInfo(user) {
   document.getElementById("username").innerHTML = user.username;
-  document.getElementById("level").innerHTML = "Level: " + (Math.floor(user.num_bottles / 5) + 1);
-  document.getElementById("bottles_filled").innerHTML = "Bottles Filled: " + user.num_bottles;
+  document.getElementById("level").innerHTML =
+    "Level: " + (Math.floor(user.num_bottles / 5) + 1);
+  document.getElementById("bottles_filled").innerHTML =
+    "Bottles Filled: " + user.num_bottles;
   document.getElementById("name").innerHTML = user.name;
   // document.getElementById("modal_name").innerHTML;
-
 }
 
-function displayProfilePic(id)
-{
+function displayProfilePic(id) {
   if (id == document.getElementById("1").id) {
     document.getElementById("profile-pic").src = "./assets/colorEarth.jpg";
   }
   if (id == document.getElementById("2").id) {
-    document.getElementById("profile-pic").src = "./assets/envi.png";
+    document.getElementById("profile-pic").src = "./assets/recycling.jpg";
   }
   if (id == document.getElementById("3").id) {
-    document.getElementById("profile-pic").src = "./assets/environmentalist.jpg";
+    document.getElementById("profile-pic").src =
+      "./assets/environmentalist.jpg";
   }
   if (id == document.getElementById("4").id) {
     document.getElementById("profile-pic").src = "./assets/flatirons.png";
@@ -122,17 +125,27 @@ function displayProfilePic(id)
     document.getElementById("profile-pic").src = "./assets/plant.jpg";
   }
   if (id == document.getElementById("10").id) {
-    document.getElementById("profile-pic").src = "./assets/recycling.jpeg";
+    document.getElementById("profile-pic").src = "./assets/flatirons.png";
   }
 }
 
 function updateInfo() {
-  if (document.getElementById("modal_name").innerHTML != document.getElementById("name")) {
-    document.getElementById("name").innerHTML = document.getElementById("modal_name").value;
+  if (
+    document.getElementById("modal_name").innerHTML !=
+    document.getElementById("name")
+  ) {
+    document.getElementById("name").innerHTML = document.getElementById(
+      "modal_name"
+    ).value;
   }
 
-  if (document.getElementById("modal_username").innerHTML != document.getElementById("username")) {
-    document.getElementById("username").innerHTML = document.getElementById("modal_username").value;
+  if (
+    document.getElementById("modal_username").innerHTML !=
+    document.getElementById("username")
+  ) {
+    document.getElementById("username").innerHTML = document.getElementById(
+      "modal_username"
+    ).value;
   }
 }
 
@@ -142,28 +155,27 @@ function onClick() {
 }
 
 function loadProfile() {
-
   // get user data from test server
   if (document.cookie.length > 0) {
     // Get username from cookie
-    let username = document.cookie.split(';')[0].split('=')[1]
+    let username = document.cookie.split(";")[0].split("=")[1];
     axios
-    .get("http://localhost:5000/users/" + username)
-    .then(function (response) {
-      // handle success
-      console.log(response);
-      var user = response.data;
-      displayPicture(user);
-      displayAchievements(user);
-      displayInfo(user);
-    })
-    .catch(function (error) {
-      loadExampleUser()
-      // handle error
-      console.log(error);
-    })
+      .get("http://localhost:5000/users/" + username)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        var user = response.data;
+        displayPicture(user);
+        displayAchievements(user);
+        displayInfo(user);
+      })
+      .catch(function (error) {
+        loadExampleUser();
+        // handle error
+        console.log(error);
+      });
   } else {
-    loadExampleUser()
+    loadExampleUser();
   }
 }
 
@@ -174,4 +186,3 @@ function loadExampleUser() {
   displayAchievements(exUser);
   displayInfo(exUser);
 }
-
