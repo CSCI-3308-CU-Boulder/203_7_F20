b10e1017dcdf3b7027c9ed3d9a6b6a17b42c38f2
Brian Mayers <brma8888@colorado.edu>
2020-10-27 19:38:20 -0600

    Working on username route

--- envi-api/src/routes/username.ts
+++ envi-api/src/routes/username.ts
@@ -1,2 +1,14 @@
 const Router = require('express-promise-router')
 const query = require('../db')
+
+const router = Router
+
+//fetches a username from user id
+router.get('/username', async (req, res) =>
+{
+    var user_id = req.query.id;
+    var username_query = `select username from users where id='{user_id}'`;
+})
+
+module.exports = router
+export {} 
