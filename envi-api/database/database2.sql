f23125e1cd694aa4db93fe67942f9bdc25a94c8b
sama9843 <sama9843@colorado.edu>
2020-11-03 16:54:10 -0700

    Fixed database sql

--- envi-api/database/database2.sql
+++ envi-api/database/database2.sql
@@ -1,5 +1,5 @@
 -- creates user profile table
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY, 
     username VARCHAR NOT NULL UNIQUE, 
     password VARCHAR NOT NULL, 
@@ -13,43 +13,45 @@ CREATE TABLE users (
 );
 
 -- -- creates linking table to array of friends
-CREATE TABLE friend_link (
+CREATE TABLE IF NOT EXISTS friend_link (
     user_id INT NOT NULL, 
     friends_id_array int[], 
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE actions_link (
+CREATE TABLE IF NOT EXISTS actions_link (
     user_id INT NOT NULL, 
     actions_id_array int[], 
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE achievments (
+CREATE TABLE IF NOT EXISTS achievments (
     user_id INT NOT NULL, 
     achievments_id_array int[], 
-    FOREIGN KEY(id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE  achievments_list (
+CREATE TABLE  IF NOT EXISTS achievments_list (
     achievments_id INT PRIMARY KEY NOT NULL, 
     achievment_name VARCHAR NOT NULL
 );
 
-CREATE TABLE actions_list (
+CREATE TABLE IF NOT EXISTS actions_list (
     actions_id INT NOT NULL, 
     action_name VARCHAR NOT NULL
 );
 
-CREATE TABLE company (
-    company_id INT PRIMARY KEY NOT NULL
+CREATE TABLE IF NOT EXISTS company (
+    company_id INT PRIMARY KEY NOT NULL,
     company_name VARCHAR(100) NOT NULL,
     description VARCHAR(1000),
     url VARCHAR(100)
 );
 
-CREATE TABLE donations (
-    FOREIGN KEY(id) REFERENCES users(id),
+CREATE TABLE IF NOT EXISTS donations (
+    user_id INT NOT NULL,
+    company_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(company_id) REFERENCES company(company_id),
     amount DECIMAL NOT NULL,
     message VARCHAR(100)
