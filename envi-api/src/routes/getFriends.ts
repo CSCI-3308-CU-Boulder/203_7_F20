d9d83f2f12aa1782989a4b70e2af05f5a89afc51
Sarah Zendle <sarahzendle@Sarahs-MacBook-Pro-3.local>
2020-11-03 19:58:56 -0700

    getFriends route description

---
+++ envi-api/src/routes/getFriends.ts
@@ -0,0 +1,16 @@
+/*
+
+input: username and/or user id
+returns an array of objects:
+    data = [
+        {
+            username (string),
+            name (string),
+            image_id (int),
+            level (int)
+        },
+        etc.
+    ]
+
+
+*/
