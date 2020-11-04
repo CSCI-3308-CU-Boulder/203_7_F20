c88a216f9b3e4e9219a55db308695a05292efc73
Brian Mayers <brma8888@colorado.edu>
2020-11-03 20:14:02 -0700

    Added query for updating action table

--- envi-api/src/routes/completeTasks.ts
+++ envi-api/src/routes/completeTasks.ts
@@ -1,15 +1,26 @@
 const Router = require('express-promise-router')
 const query = require('../db')
+var pgp = require('pg-promise')();
 
-const router = new Router
+const router = new Router()
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'envi_db',
+	user: 'postgres',
+	password: 'pwd'
+};
 
 router.get('/username/:completeTasks', async (req, res) =>
 {
-    const {achievment} = req.params
-    console.log(query)
-    const {rows} = await query('SELECT achievments_id FROM achievments_list')
-    res.send(rows)
+    var actionsID = req.query.action_id; //example name
+    var actionDescript = req.query.action_description;
+    var update = `INSERT INTO action_list (action_id, action_name) VALUES ('${actionsID}', '${actionDescript}')`;
+    db.query(update, function (error, results, fields) {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    });
 })
-
+var db = pgp(dbConfig);
 module.exports = router
 export {}
