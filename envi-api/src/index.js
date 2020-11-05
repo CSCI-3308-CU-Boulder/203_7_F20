590f824110eb60ab04d54f807af362db5681aad5
Brian Mayers <brma8888@colorado.edu>
2020-11-04 19:02:07 -0700

    updated task route

---
+++ envi-api/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const {pool} = require('./config')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addBook = (request, response) => {
+  const {author, title} = request.body
+
+  pool.query(
+    'INSERT INTO books (author, title) VALUES ($1, $2)',
+    [author, title],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({status: 'success', message: 'Book added.'})
+    },
+  )
+}
+
+app
+  .route('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
