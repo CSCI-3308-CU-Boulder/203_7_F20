0ba900ba1bf07f27ae1cff0000d20ebb5fafb144
Brian Mayers <brma8888@colorado.edu>
2020-11-17 20:02:02 -0700

    changed naming to reflect html file

--- envi-api/src/routes/completeTasks.ts
+++ envi-api/src/routes/completeTasks.ts
@@ -4,14 +4,13 @@ const { ensureAuthenticated } = require('../config/auth');
 
 const router = new Router()
 
-router.use('/id:')
 
 router.post('/username/:completeTasks', async (req, res) =>
 {
-    var actionsID = req.body.action_id; //example name
+    //var actionsID = req.body.action_id; Handled in database
     var actionName = req.body.taskName;
     var actionDescript = req.body.taskDescription;
-    var update = `INSERT INTO task_list (task_id, task_name, task_description) VALUES ('${actionsID}', '${actionName}' '${actionDescript}')`;
+    var update = `INSERT INTO task_list (task_name, task_description) VALUES ('${actionName}' '${actionDescript}')`;
     query(update, function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results));
