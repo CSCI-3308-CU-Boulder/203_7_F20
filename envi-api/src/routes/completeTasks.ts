eb39fa3eeb64d1e35b5132e16c53f6ac523ce27a
Brian Mayers <brma8888@colorado.edu>
2020-11-17 20:10:09 -0700

    added task type and new info based on updated database

--- envi-api/src/routes/completeTasks.ts
+++ envi-api/src/routes/completeTasks.ts
@@ -8,9 +8,11 @@ const router = new Router()
 router.post('/username/:completeTasks', async (req, res) =>
 {
     //var actionsID = req.body.action_id; Handled in database
+    var user_id = req.user.id;
     var actionName = req.body.taskName;
     var actionDescript = req.body.taskDescription;
-    var update = `INSERT INTO task_list (task_name, task_description) VALUES ('${actionName}' '${actionDescript}')`;
+    var taskType = req.body.type;
+    var update = `INSERT INTO task_list (user_id, name, description, completed, type) VALUES ('${user_id}', ${actionName}', '${actionDescript}', TRUE, '${taskType}')`;
     query(update, function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results));
