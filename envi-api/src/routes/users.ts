219da45941a9d446a63cce020f48ba5213e285b4
RebeccaCoryell <reco7397@colorado.edu>
2020-11-21 16:32:44 -0700

    added new routes and adjusted completeTask route

--- envi-api/src/routes/users.ts
+++ envi-api/src/routes/users.ts
@@ -87,17 +87,17 @@ router.get('/:id/', (req, res) => {
 })
 
 // Update profile info
-router.post('/:id/updateInfo', ensureAuthenticated, function(req, res) {
-    var newUsername = req.body.modal_username.toLowerCase();
+router.post('/:username/updateInfo', ensureAuthenticated, function(req, res) {
+    var newUsername = (req.body.modal_username).toLowerCase();
     var newName = req.body.modal_name;
     var newImage = req.body.modal_image_id;
-    var newEmail = req.body.defaultForm_email.toLowerCase();
-    var updateQuery = "UPDATE users SET username ='" +newUsername+ "', name ='" +newName+"', image_id =" +newImage+ ", email ='" +newEmail+ "' WHERE id =$1";
+    var newEmail = (req.body.defaultForm_email).toLowerCase();
+    var updateQuery = "UPDATE users SET username ='" +newUsername+ "', name ='" +newName+"', image_id =" +newImage+ ", email ='" +newEmail+ "' WHERE username=$1";
     console.log(updateQuery);
     console.log(req.params);
-    let { id } = req.params;
-    console.log(id);
-    query(updateQuery, [id], function (error, results, fields) {
+    let { username } = req.params;
+    // console.log(username);
+    query(updateQuery, [username], function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results));
     });
@@ -186,17 +186,75 @@ router.post('/:id/addFriend/:friendUsername', function(req,res) {
     // });
 })
 
-router.post('/username/:completeTasks', async (req, res) =>
-{
+// completeTasks -- NOT TESTED
+router.post('/username/:completeTasks', async (req, res) => { // json with user_id and task_id
     //var actionsID = req.body.action_id; Handled in database
-    var user_id = req.params.id;
-    var actionName = req.body.taskName;
-    var actionDescript = req.body.taskDescription;
-    var taskType = req.body.type;
-    var update = `INSERT INTO task_list (user_id, name, description, completed, type) VALUES ('${user_id}', ${actionName}', '${actionDescript}', TRUE, '${taskType}')`;
-    query(update, function (error, results, fields) {
+    // var user_id = req.params.id;
+    // var actionName = req.body.taskName;
+    // var actionDescript = req.body.taskDescription;
+    // var taskType = req.body.type;
+    const { user_id, task_id } = req.body;
+    var update = "UPDATE tasks SET completed = completed + 1 WHERE user_id = $1, id=$2;"; // updating the task as completed in db
+    var points = "UPDATE users SET impact_points = impact_points + 1 WHERE user_id = $1"; // adding impact points for the user
+    query(update, [user_id, task_id], function(error, results, fields) {
         if (error) throw error;
-        res.send(JSON.stringify(results));
+        console.log('Task updated');
+        query(points, [user_id], function(error1, results1, fields) {
+            if (error1) throw error1;
+            console.log('User points updated');
+            res.json({success: true});
+        })
+    });
+})
+
+
+// addTask -- NOT TESTED -- used for non static requests
+router.post('/:id/addTask', function(req, res) { // parameters -- name, description, impact
+    console.log('Adding task');
+    const { user_id, name, description, impact } = req.body;
+    if(!user_id || !name || !impact) {
+        res.status(401).send("Invalid task input")
+        return null
+    }
+    var taskQuery = "INSERT INTO tasksList (name, description, impact) VALUES ($1, $2, $3);"; // adding task to list
+    query(taskQuery, [name, description, impact], function(error, results) {
+        if (error) throw error;
+        res.json({success : true});
+    });
+})
+
+
+// addAchievement -- NOT TESTED
+router.post('/:id/addAchievement', function(req, res) { // parameters -- name, image_id, descriptions, user_id
+    console.log('Adding achievement');
+    const { user_id, name, description, image_id } = req.body;
+    if(!user_id || !name || !image_id) {
+        res.status(401).send("Invalid Achievement");
+        return null;
+    }
+    var achQuery = "INSERT INTO achievments (user_id, name, description, image_id) VALUES ($1, $2, $3, $4)";
+    query(achQuery, [user_id, name, description, image_id], function(error, results) {
+        if (error) throw error;
+        res.json({success : true});
+    });
+})
+
+// numTasks -- NOT TESTED -- used for statistics (if implemented)
+router.get('/id:/numTasks', function(req, res) {
+    let { user_id } = req.params.id;
+    var reuseQuery = "SELECT COUNT(times_completed) FROM tasks WHERE user_id = " +user_id+ ", impact='reuse'";
+    var reduceQuery = "SELECT COUNT(times_completed) FROM tasks WHERE user_id = " +user_id+ ", impact='reduce'";
+    var recycleQuery = "SELECT COUNT(times_competed) FROM tasks WHERE user_id = " +user_id+ ", impact='recycle'";
+    query(reuseQuery, function(error1, results1) {
+        if(error1) throw error1;
+        query(reduceQuery, function(error2, results2) {
+            if (error2) throw error2;
+            query(recycleQuery, function (error3, results3) {
+                if (error3) throw error3;
+                res.json({reuse: results1, reduce: results2, recycle: results3}); // need to check on like actual results -- idk about the format
+                // res.send(JSON.stringify({reuse: results1, reduce: results2, recycle: results3})); // maybe this actually oop idk
+            })
+        });
     });
 })
 
